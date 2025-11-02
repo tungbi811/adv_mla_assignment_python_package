4691dfb3319beabc6c946359fb89d14354933122
tungbi811 <tungduynguyen811@gmail.com>
2025-11-03 10:59:05 +1100

    udpate at3

--- src/amla_at1_25608516/models/evaluate.py
+++ src/amla_at1_25608516/models/evaluate.py
@@ -12,8 +12,28 @@ from sklearn.metrics import (
     confusion_matrix,
     ConfusionMatrixDisplay
 )
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
-def evaluate_model(model, X_val, y_val, labels=["Not Drafted", "Drafted"]):
+
+def evaluate_regression_model(y_true, y_pred):
+    mae = mean_absolute_error(y_true, y_pred)
+    rmse = np.sqrt(mean_squared_error(y_true, y_pred))
+    r2 = r2_score(y_true, y_pred)
+
+    # Print results
+    print("==== Evaluation Results (Regression) ====")
+    print(f"MAE:  {mae:.4f}")
+    print(f"RMSE: {rmse:.4f}")
+    print(f"R² Score: {r2:.4f}")
+
+    return {
+        "MAE": mae,
+        "RMSE": rmse,
+        "R2": r2
+    }
+
+
+def evaluate_classification_model(model, X_val, y_val, labels=["Not Drafted", "Drafted"]):
     """
     Evaluate a trained model on a validation dataset.
 
